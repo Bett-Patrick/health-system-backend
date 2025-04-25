b5d308fbdbb010ab573436e03b94cb964c00deb6
Bett <bettpatrick080@gmail.com>
2025-04-26 01:32:59 +0300

    feat : register-admin endpoint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask ;
+from flask import Flask, request, make_response ;
 from flask_migrate import Migrate
 from flask_restful import Resource,Api
-from models import db
+from models import db, bcrypt, User, UserRole
 
 
 app = Flask(__name__)
@@ -11,13 +11,51 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+bcrypt.init_app(app)
 api = Api(app)
 
 
 # Routes
+
+# Home Resource
 @app.route("/")
 def index():
-    return "<h1>Welcome to Health Information System(HIS) API</h1>"
+    return make_response("<h1>Welcome to Health Information System(HIS) API</h1>", 200)
+
+# RegisterAdmin Resource
+class RegisterAdmin(Resource):
+    def post(self):
+        if User.query.first():
+            return make_response({"error":"Admin already exists"}, 403)
+        
+        data = request.json
+        # Extract data from the request
+        username = data.get("username")
+        email = data.get("email")
+        password = data.get("password")
+        
+        if not all([username, email, password]):
+            return make_response({"error" : "Missing required fields"}, 400)
+        
+        user = User(
+            username = username,
+            email = email,
+            role = UserRole.ADMIN
+        )
+        user.set_password(password)
+        
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return make_response({"message" : "Admin registered successfully"}, 200)
+        except Exception as e:
+            db.session.rollback()
+            return make_response({"error" : str(e)}, 500)
+        
+api.add_resource(RegisterAdmin, "/register-admin")
+        
+    
+    
 
 if __name__ == '__main__':
     app.run(port = 5555, debug=True)
--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy;
 from sqlalchemy_serializer import SerializerMixin;
+from flask_bcrypt import Bcrypt
 import enum
 
-db = SQLAlchemy();
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 class UserRole(enum.Enum):
     ADMIN = "admin"
@@ -12,7 +14,19 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable = False, unique = True)
-    email = db.Column(db.String, nullable = False, unique = True)
+    username = db.Column(db.String(120), nullable = False, unique = True)
+    email = db.Column(db.String(120), nullable = False, unique = True)
+    password_hash = db.Column(db.String, nullable = False)
     role = db.Column(db.Enum(UserRole), nullable = False)
-    password = db.Column(db.String, nullable = False)
+    
+    # Hash the password before saving it to the database
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    # Check if the given password matches the stored password hash
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+    
+    def __repr__(self):
+        return f"<User {self.username} {self.email}>"
