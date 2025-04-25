11ca85a6981deb489ce12a61eb08414c536bd8a0
Bett <bettpatrick080@gmail.com>
2025-04-25 21:32:50 +0300

    Add UserRole enum and User model via migration

--- app.py
+++ app.py
@@ -13,5 +13,11 @@ migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
 
+
+# Routes
+@app.route("/")
+def index():
+    return "<h1>Welcome to Health Information System(HIS) API</h1>"
+
 if __name__ == '__main__':
     app.run(port = 5555, debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy;
 from sqlalchemy_serializer import SerializerMixin;
+import enum
 
 db = SQLAlchemy();
+
+class UserRole(enum.Enum):
+    ADMIN = "admin"
+    DOCTOR = "doctor"
+    
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable = False, unique = True)
+    email = db.Column(db.String, nullable = False, unique = True)
+    role = db.Column(db.Enum(UserRole), nullable = False)
+    password = db.Column(db.String, nullable = False)
